04bd544805fed17c23410423c15f2bcf68cfe811
Benjamin Decoster <bdcstr@gmail.com>
2022-06-16 17:55:42 +0200

    init

--- app/routes/$lang/home.tsx
+++ app/routes/$lang/home.tsx
@@ -16,9 +16,13 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>{t("greeting")}</h1>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-96">
         <Link to="book-1">Book 1</Link>
         <Link to="book-2">Book 2</Link>
+        <Link to="book-3">Book 3</Link>
+        <Link to="book-4">Book 4</Link>
+        <Link to="book-5">Book 5</Link>
+        <Link to="book-6">Book 6</Link>
       </div>
       <Outlet />
     </div>
--- app/routes/$lang/home/$book.tsx
+++ app/routes/$lang/home/$book.tsx
@@ -1,7 +1,7 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { json, LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { Fragment, useState } from "react";
+import { useLoaderData, useLocation, useNavigate } from "@remix-run/react";
+import { Fragment, useEffect, useState } from "react";
 import i18next from "~/i18next.server";
 
 type LoaderData = { locale: string; page: string };
@@ -15,7 +15,13 @@ export default function MyModal() {
   const { page } = useLoaderData<LoaderData>();
   let [isOpen, setIsOpen] = useState(true);
 
+  const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => openModal(), [location.pathname]);
+
   function closeModal() {
+    console.log("closeModal");
+    navigate(-1);
     setIsOpen(false);
   }
 
@@ -25,67 +31,34 @@ export default function MyModal() {
 
   return (
     <>
-      <div className="fixed inset-0 flex items-center justify-center">
-        <button
-          type="button"
-          onClick={openModal}
-          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-        >
-          Open dialog
-        </button>
-      </div>
+      <Dialog as="div" className="relative z-10" open={true} onClose={closeModal}>
+        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                {page.toUpperCase()}
+              </Dialog.Title>
+              <div className="mt-2">
+                <p className="text-sm text-gray-500">
+                  Your payment has been successfully submitted. We’ve sent you an email with all of the details of your
+                  order.
+                </p>
+              </div>
 
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                    {page.toUpperCase()}
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Your payment has been successfully submitted. We’ve sent you an email with all of the details of
-                      your order.
-                    </p>
-                  </div>
-
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      Got it, thanks!
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
+              <div className="mt-4">
+                <button
+                  type="button"
+                  className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  onClick={closeModal}
+                >
+                  Got it, thanks!
+                </button>
+              </div>
+            </Dialog.Panel>
           </div>
-        </Dialog>
-      </Transition>
+        </div>
+      </Dialog>
     </>
   );
 }
